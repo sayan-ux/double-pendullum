b96cac83b98550a1706b906e6e47305d057e86f6
sayan-ux <sayansikdar636@gmail.com>
2020-03-19 21:14:37 +0530

    initial commit

---
+++ pendulum .js
@@ -0,0 +1,69 @@
+let g = 1;
+let pi = 3.1421;
+let ar = 0;
+//data of forst pendulum
+let r1 = 150;
+let a1 = pi / 2;
+let m1 = 40;
+let x1 = 0, y1 = 0;
+let a1v = 0, a1a = 0;
+//data of second pendulum
+let r2 = 150;
+let a2 = pi / 2;
+let m2 = 40;
+let x2 = 0, y2 = 0;
+let a2v = 0.09, a2a = 0.09;
+
+let history
+function setup() {
+    createCanvas(600, 600);
+
+}
+
+
+function draw() {
+    background(150);
+    translate(300, 100);
+
+
+
+    a1a = (-g * (2 * m1 + m2) * sin(a1) - m2 * g * sin(a1 - 2 * a2) - 2 * sin(a1 - a2) * m2 * (a2v * a2v * r2 + a1v * a1v * r1 * cos(a1 - a2))) / (r1 * (2 * m1 + m2 - m2 * cos(2 * a1 - 2 * a2)));
+
+    a2a = (2 * sin(a1 - a2) * (a1v * a1v * r1 * (m1 + m1) + g * (m1 + m2) * cos(a1) + a2v * a2v * r2 * m2 * cos(a1 - a2))) / (r2 * (2 * m1 + m2 - m2 * cos(2 * a1 - 2 * a2)));
+
+    stroke(80, 23, 43);
+    strokeWeight(4);
+
+    x1 = r1 * sin(a1);
+    y1 = r1 * cos(a1);
+
+    (2 * m1 + m2 - m2 * cos(2 * a1 - 2 * a2))
+    x2 = x1 + r2 * sin(a2);
+    y2 = y1 + r2 * cos(a2);
+
+    line(0, 0, x1, y1);
+    line(x1, y1, x2, y2);
+    circle(x1, y1, m1);
+    circle(x2, y2, m2);
+
+    a1v += a1a;
+    a2v += a2a;
+    a1 += a1v;
+    a2 += a2v;
+    ar = random(0.00003, 0.00005);
+    if (a1v > 0) {
+        a1v -= ar;
+    }
+    else {
+        a1v += ar;
+    }
+    if (a2v > 0) {
+        a2v -= ar;
+    }
+    else {
+        a2v += ar;
+    }
+
+
+
+}
